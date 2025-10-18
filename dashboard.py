3c9c21c46eeb121c27036d0cca4a65e86b52e8b5
ojaaahusna-ctrl <ojaaahusna@gmail.com>
2025-10-18 10:32:19 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,7 @@ import cv2
 @st.cache_resource
 def load_models():
     yolo_model = YOLO("model/best.pt")  # Model deteksi objek
-    classifier = tf.keras.models.load_model("model/classifier_model.h5")  # Model klasifikasi
+    classifier = tf.keras.models.load_model("model/Raudhatul Husna_laporan2.h5")  # Model klasifikasi
     return yolo_model, classifier
 
 yolo_model, classifier = load_models()
@@ -31,19 +31,16 @@ if uploaded_file is not None:
     st.image(img, caption="Gambar yang Diupload", use_container_width=True)
 
     if menu == "Deteksi Objek (YOLO)":
-        # Deteksi objek
         results = yolo_model(img)
-        result_img = results[0].plot()  # hasil deteksi (gambar dengan box)
+        result_img = results[0].plot()
         st.image(result_img, caption="Hasil Deteksi", use_container_width=True)
 
     elif menu == "Klasifikasi Gambar":
-        # Preprocessing
-        img_resized = img.resize((224, 224))  # sesuaikan ukuran dengan model kamu
+        img_resized = img.resize((224, 224))
         img_array = image.img_to_array(img_resized)
         img_array = np.expand_dims(img_array, axis=0)
         img_array = img_array / 255.0
 
-        # Prediksi
         prediction = classifier.predict(img_array)
         class_index = np.argmax(prediction)
         st.write("### Hasil Prediksi:", class_index)
